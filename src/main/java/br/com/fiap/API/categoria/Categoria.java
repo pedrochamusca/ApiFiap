efdf63f40adb93bfa4a0356b95633549d2bd9470
pedrochamusca <chamusca2002@gmail.com>
2026-03-25 19:44:45 -0300

    Criação da Classe Categoria / Atualizações de credenciais do banco

---
+++ src/main/java/br/com/fiap/API/categoria/Categoria.java
@@ -0,0 +1,41 @@
+package br.com.fiap.API.categoria;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "categorias")
+@Entity(name = "Categoria")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private int ativo;
+
+    public Categoria(DadosCadastroCategoria dados){
+        this.nome = dados.nome();
+        this.descricao = dados.descricao();
+        this.ativo = 1;
+    }
+
+    public void atualizarCategoria(@Valid DadosAtualizarCategoria dados) {
+        if(dados.nome() != null)
+            this.nome = dados.nome();
+        if(dados.descricao() != null)
+            this.descricao = dados.descricao();
+    }
+
+    public void excluirCategoria() {
+        this.ativo = 0;
+    }
+}
